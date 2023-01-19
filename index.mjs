51c6dd11f8c9042e0393005326c812f0f5d2a86f
ShehzadQadri <63380192+ShehzadQadri@users.noreply.github.com>
2023-01-20 01:58:55 +0500

    updated code

---
+++ index.mjs
@@ -0,0 +1,21 @@
+import express from "express";
+import path from 'path';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/abc", (req, res) => {
+    console.log("Requested ip: " + req.ip + new Date());
+    res.send("Hello World!" + new Date());
+});
+
+const __dirname = path.resolve();
+
+
+app.use('/', express.static(path.join(__dirname, './web/build')));
+
+app.listen(port, () => {
+    console.log("Server is running on port: " + port);
+})
