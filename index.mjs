91159ae87ceea645e40ade9c38ed071792357e5f
ShehzadQadri <63380192+ShehzadQadri@users.noreply.github.com>
2023-01-21 03:21:19 +0500

    push my react js app onto nodejs

--- index.mjs
+++ index.mjs
@@ -4,18 +4,26 @@ import path from 'path';
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(express.json());
+// app.use(express.json());
 
-app.get("/abc", (req, res) => {
-    console.log("Requested ip: " + req.ip + new Date());
-    res.send("Hello World!" + new Date());
-});
+// app.use('/', (req, res) => {})
 
 const __dirname = path.resolve();
-
-
 app.use('/', express.static(path.join(__dirname, './web/build')));
+app.use('*', express.static(path.join(__dirname, './web/build')));
+
+
+app.get("/time", (req, res) => {
+    console.log("Requested ip: " + req.ip + new Date());
+    res.send("Hello World!" + new Date().toString());
+});
+
+// const __dirname = path.resolve();
+
+
+// app.use('/', express.static(path.join(__dirname, './web/build')));
 
 app.listen(port, () => {
+    console.log("Server is running on port: " + port);
     console.log("Server is running on port: " + port);
 })
